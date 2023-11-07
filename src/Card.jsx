625840d8de813e690c85ee27a9f1fb16c4ef09f5
BenPet <bendzuke@gmail.com>
2023-11-07 15:43:46 +0200

    whatever

--- src/Card.jsx
+++ src/Card.jsx
@@ -2,8 +2,7 @@ const Card = (props) => {
   return (
     <div className="card">
       <span class="material-symbols-outlined">close</span>
-      <p id="cardName">Name: {props.name}</p>
-      <p id="cardType">Type: {props.type}</p>
+      <p id="cardName">{props.name}</p>
       <img src="https://source.unsplash.com/400x400/?fox" alt="picture" />
       <p id="cardLikes">
         <span class="material-symbols-outlined">thumb_up</span>
